05f067088f119dd6eaa6455f801652addc67a133
Jeremy Morgan <galedragon1223@gmail.com>
2013-04-28 13:53:55 -0400

    Used more of bs4's functionality than the re module

--- anansi.py
+++ anansi.py
@@ -13,19 +13,25 @@ try:
 	import ConfigParser as cp #Python 2
 except ImportError:
 	import configparser as cp #Python 3
-import re
 
 class SQLiTest(object):
-	def __init__(self, page):
+	def __init__( self, page ):
 		'''Page is HTML, as string'''
-		self.page = page
-		print page
+		self.page = BeautifulSoup( page )
+		self.inputs = self._find_input( )
 		
-	def _find_input(self):
-		ind = 0
-		while ind != -1:
-			ind = self.page.find("input")
-			
+	def _find_input( self ):
+		matches = self.page.find_all('input')
+		varNames = dict()
+		for m in matches:
+			if m.has_key('name'):
+				try:
+					dummy = m['type']
+				except KeyError:
+					varNames[m['name']] = m["value"]
+		return varNames
+		
+				
 	def _in_band(self):
 		pass
 		
@@ -42,12 +48,13 @@ class SQLiTest(object):
 def main():
 	config = cp.ConfigParser()
 	config.read('SQLi/sqli.config')
-	print config.get('SQL','rooting')
+	#print config.get('SQL','rooting')
 	
 if __name__=="__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-w",help="The web address")
 	args = parser.parse_args()
-	s_page = requests.get( args.w )
+	s_page = requests.post( args.w )
+	print BeautifulSoup(s_page.text).prettify()
 	s = SQLiTest( s_page.text )
 	main()
